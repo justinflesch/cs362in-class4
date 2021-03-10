df9ac1c3c2b44353de97f2b9d7e21f5d333177fa
justinflesch <fleschju@oregonstate.edu>
2021-03-10 08:44:54 -0800

    First commit

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,9 +5,9 @@ class TestCaseCalc(unittest.TestCase):
     def test_valid(self):
         self.assertEqual(fibonacci.fib(0), 0)
         self.assertEqual(fibonacci.fib(1), 1)
-	self.assertEqual(fibonacci.fib(2), 2)
-	self.assertEqual(fibonacci.fib(3), 3)
-	self.assertEqual(fibonacci.fib(5), 8)
+        self.assertEqual(fibonacci.fib(2), 2)
+        self.assertEqual(fibonacci.fib(3), 3)
+        self.assertEqual(fibonacci.fib(5), 8)
 
 if __name__ == "__main__":
     unittest.main()
